b3f4882044a0a5f1d2cf6c6ae29bff48be02ac18
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-02-05 08:06:18 -0300

    feat: begin implementating the authentication app

---
+++ apps/server/src/features/authentication/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('token/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', views.TokenRefreshView.as_view(), name='token_refresh'),
+]
---
+++ apps/server/src/features/authentication/views.py
@@ -0,0 +1,10 @@
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView as TokenObtainPairViewBase,
+    TokenRefreshView as TokenRefreshViewBase,
+)
+
+class TokenObtainPairView(TokenObtainPairViewBase):
+    pass
+
+class TokenRefreshView(TokenRefreshViewBase):
+    pass
