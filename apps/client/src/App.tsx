3a14351f7202913b9814d7439fd519ad5b2bde50
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-02-07 15:35:16 -0300

    feat: implements base of notes app

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,17 +1,60 @@
 import { useState } from 'react'
-
+import Grid from '@mui/material/Grid';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
 import { MarkdownEditor } from '@backoffice/md-editor';
 
 function App() {
   const [markdown, setMarkdown] = useState('# hello world');
 
   return (
-    <>
-      <MarkdownEditor
-        value={markdown}
-        onChange={setMarkdown}
-      />
-    </>
+    <Grid container spacing={2} sx={{ height: '100vh' }}>
+      <Grid item xs={2}>
+        <List sx={{ width: '100%', maxWidth: '100%', bgcolor: 'background.paper' }}>
+          <ListItem alignItems="flex-start">
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <>
+                  I'll be in your neighborhood doing errands this...
+                </>
+              }
+            />
+          </ListItem>
+          <Divider component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <>
+                  I'll be in your neighborhood doing errands this...
+                </>
+              }
+            />
+          </ListItem>
+          <Divider component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <>
+                  I'll be in your neighborhood doing errands this...
+                </>
+              }
+            />
+          </ListItem>
+        </List>
+      </Grid>
+      <Grid item xs={10}>
+        <MarkdownEditor
+          value={markdown}
+          onChange={setMarkdown}
+        />
+      </Grid>
+    </Grid>
   );
 };
 
