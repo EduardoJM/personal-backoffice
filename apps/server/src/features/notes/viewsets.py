a4002b175cea70b35e0beb9531605e6a23095261
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-02-11 22:06:19 -0300

    feat: implements pagination and one more info

--- apps/server/src/features/notes/viewsets.py
+++ apps/server/src/features/notes/viewsets.py
@@ -1,6 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.renderers import BaseRenderer
 from .models import Note
 from .serializers import NoteSerializer
+
+
+class MarkdownRenderer(BaseRenderer):
+    media_type = "text/markdown"
+    format = "md"
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        return data
 
 
 class NoteViewSet(ModelViewSet):
@@ -8,3 +19,8 @@ class NoteViewSet(ModelViewSet):
 
     def get_queryset(self):
         return Note.objects.filter(user=self.request.user).order_by("-updated_at").all()
+
+    @action(methods=["GET"], detail=True, renderer_classes=[MarkdownRenderer])
+    def text(self, request, pk=None):
+        note = self.get_object()
+        return Response(note.text)
