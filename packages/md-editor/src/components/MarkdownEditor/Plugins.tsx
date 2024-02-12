ff920d34ed896ade41e8523c5229d47e57b0b703
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-02-11 22:37:16 -0300

    refactor: change toolbar

--- packages/md-editor/src/components/MarkdownEditor/Plugins.tsx
+++ packages/md-editor/src/components/MarkdownEditor/Plugins.tsx
@@ -1,22 +1,44 @@
 import {
-  headingsPlugin,
+  toolbarPlugin,
+  KitchenSinkToolbar,
   listsPlugin,
   quotePlugin,
+  headingsPlugin,
+  linkPlugin,
+  linkDialogPlugin,
+  imagePlugin,
+  tablePlugin,
   thematicBreakPlugin,
-  toolbarPlugin,
+  frontmatterPlugin,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  directivesPlugin,
+  AdmonitionDirectiveDescriptor,
   diffSourcePlugin,
+  markdownShortcutPlugin,
 } from '@mdxeditor/editor';
 import { Toolbar } from './Toolbar';
 
 export const getPlugins = (lastSavedText: string) => {
   return [
-    headingsPlugin(),
-    listsPlugin(),
-    quotePlugin(),
-    thematicBreakPlugin(),
     toolbarPlugin({
       toolbarContents: () => <Toolbar />,
     }),
+    listsPlugin(),
+    quotePlugin(),
+    headingsPlugin(),
+    linkPlugin(),
+    linkDialogPlugin(),
+    imagePlugin({ imageUploadHandler: async () => '/sample-image.png' }),
+    tablePlugin(),
+    thematicBreakPlugin(),
+    frontmatterPlugin(),
+    codeBlockPlugin({ defaultCodeBlockLanguage: 'txt' }),
+    codeMirrorPlugin({
+      codeBlockLanguages: { js: 'JavaScript', css: 'CSS', txt: 'text', tsx: 'TypeScript' },
+    }),
+    directivesPlugin({ directiveDescriptors: [AdmonitionDirectiveDescriptor] }),
+    markdownShortcutPlugin(),
     diffSourcePlugin({
       viewMode: 'rich-text',
       diffMarkdown: lastSavedText,
--- packages/md-editor/src/components/MarkdownEditor/Toolbar.tsx
+++ packages/md-editor/src/components/MarkdownEditor/Toolbar.tsx
@@ -7,21 +7,86 @@ import {
   CreateLink,
   DiffSourceToggleWrapper,
   Separator,
+  ConditionalContents,
+  ChangeCodeMirrorLanguage,
+  EditorInFocus,
+  DirectiveNode,
+  ChangeAdmonitionType,
+  InsertImage,
+  InsertTable,
+  InsertThematicBreak,
+  InsertCodeBlock,
+  InsertAdmonition,
 } from '@mdxeditor/editor';
+
+const whenInAdmonition = (editorInFocus: EditorInFocus | null) => {
+  const node = editorInFocus?.rootNode;
+  if (!node || node.getType() !== 'directive') {
+    return false;
+  }
+
+  const { name } = (node as DirectiveNode).getMdastNode();
+  return ['note', 'tip', 'danger', 'info', 'caution'].includes(name);
+};
 
 export const Toolbar = () => {
   return (
     <DiffSourceToggleWrapper>
-      <UndoRedo />
-      <Separator />
-      <BoldItalicUnderlineToggles />
-      <CodeToggle />
-      <Separator />
-      <ListsToggle />
-      <Separator />
-      <BlockTypeSelect />
-      <Separator />
-      <CreateLink />
+      <ConditionalContents
+        options={[
+          {
+            when: editor => editor?.editorType === 'codeblock',
+            contents: () => <ChangeCodeMirrorLanguage />,
+          },
+          {
+            fallback: () => (
+              <>
+                <UndoRedo />
+                <Separator />
+                <BoldItalicUnderlineToggles />
+                <CodeToggle />
+                <Separator />
+                <ListsToggle />
+                <Separator />
+
+                <ConditionalContents
+                  options={[
+                    { when: whenInAdmonition, contents: () => <ChangeAdmonitionType /> },
+                    { fallback: () => <BlockTypeSelect /> },
+                  ]}
+                />
+
+                <Separator />
+
+                <CreateLink />
+                <InsertImage />
+
+                <Separator />
+
+                <InsertTable />
+                <InsertThematicBreak />
+
+                <Separator />
+                <InsertCodeBlock />
+
+                <ConditionalContents
+                  options={[
+                    {
+                      when: editorInFocus => !whenInAdmonition(editorInFocus),
+                      contents: () => (
+                        <>
+                          <Separator />
+                          <InsertAdmonition />
+                        </>
+                      ),
+                    },
+                  ]}
+                />
+              </>
+            ),
+          },
+        ]}
+      />
     </DiffSourceToggleWrapper>
   );
 };
