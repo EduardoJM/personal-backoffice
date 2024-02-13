fe5ca109457a7a882179b7f94cc4810f70ea68bb
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-02-13 10:46:50 -0300

    feat: better support for all full-text-search cognitive load

--- apps/client/src/features/notes/components/NotesItem.tsx
+++ apps/client/src/features/notes/components/NotesItem.tsx
@@ -1,7 +1,9 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import { EditableLabel } from '@backoffice/ui';
 import { Note } from '../types';
+import { useSaveNoteTitle } from '../mutations/useSaveNoteTitle';
 
 export interface NotesItemProps {
   note: Note;
@@ -9,16 +11,22 @@ export interface NotesItemProps {
 
 export const NotesItem = ({ note }: NotesItemProps) => {
   const navigate = useNavigate();
+  const { hash, search } = useLocation();
+  const saveNoteTitle = useSaveNoteTitle();
 
   const handleGoTo = () => {
-    navigate(`/notes/${note.id}/`);
+    navigate({ pathname: `/notes/${note.id}/`, hash, search });
   };
 
   return (
     <ListItem alignItems="flex-start" sx={{ cursor: 'pointer' }} onClick={handleGoTo}>
       <ListItemText
-        primary={note.title}
-        secondary={<>I'll be in your neighborhood doing errands this...</>}
+        primary={
+          <EditableLabel
+            label={note.title}
+            onSave={value => saveNoteTitle.mutate({ id: note.id, title: value })}
+          />
+        }
       />
     </ListItem>
   );
--- apps/client/src/features/notes/queries/useNotes.ts
+++ apps/client/src/features/notes/queries/useNotes.ts
@@ -1,10 +1,15 @@
 import { useQuery } from 'react-query';
+import { useSearchParams } from 'react-router-dom';
 import { getNotes } from '../services';
 
 export const useNotes = () => {
-  return useQuery('notes', {
+  const [searchParams] = useSearchParams();
+
+  const search = searchParams.get('search') || '';
+
+  return useQuery(['notes', search], {
     queryFn: async () => {
-      return getNotes();
+      return getNotes({ search });
     },
   });
 };
